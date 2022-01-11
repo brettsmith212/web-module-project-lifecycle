1839c51e643d07a24735806a4884b147abfa14e9
Brett Smith <brettsmith212@gmail.com>
2022-01-11 12:10:23 -0800

    api call displaying correct data

--- src/App.js
+++ src/App.js
@@ -1,8 +1,35 @@
 import React from "react";
 import User from "./components/User";
 import FollowerList from "./components/FollowerList";
+import axios from "axios";
 
 class App extends React.Component {
+  state = {
+    user: "",
+    userInfo: {},
+    followers: [],
+  };
+
+  componentDidMount() {
+    axios
+      .get("https://api.github.com/users/brettsmith212")
+      .then((res) => {
+        console.log(res.data);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+
+    axios
+      .get("https://api.github.com/users/brettsmith212/followers")
+      .then((res) => {
+        console.log(res.data);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }
+
   render() {
     return (
       <div>
